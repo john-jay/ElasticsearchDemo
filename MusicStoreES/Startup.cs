3b8d6e5096da7a086aa42d7e59bd4671c013ae4a
John Holliday <johnmholliday@gmail.com>
2015-09-13 21:59:34 -0500

    Created a new MVC app.

---
+++ MusicStoreES/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MusicStoreES.Startup))]
+namespace MusicStoreES
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
